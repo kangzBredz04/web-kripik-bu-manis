0541600a4faf026b4932236e6083423338c87ba1
wahyu <wabredz1234@gmail.com>
2024-06-14 17:18:36 +0700

    membuat beberapa route untuk halaman customer dan admin

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Admin from "./pages/Admin.jsx";
+import ErrorPageACust from "./pages/ErrorPageCust.jsx";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPageACust />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+    // errorElement: <ErrorPageAdmin />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
