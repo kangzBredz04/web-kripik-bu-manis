c6f2b5a160660386322000d4333c8e1c7ee5c64f
wahyu <wabredz1234@gmail.com>
2024-06-15 00:47:39 +0700

    mengubah tampilan header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,29 +10,29 @@ import { SlBag } from "react-icons/sl";
 export default function Header() {
   // const { wishlist, cart } = useContext(AllContext);
   return (
-    <header className="bg-brown-dark text-white border  sticky top-0 z-50 font-pacifico">
+    <header className="bg-brown-dark text-white border  sticky top-0 z-50 font-poppins">
       <div className="container mx-auto  py-4 flex justify-between items-center">
-        <div className="flex items-center ">
+        <div className="flex items-center">
           <Link to="/" className="text-xl font-bold tracking-widest">
             KERIPIK BU MANIS
           </Link>
         </div>
-        <nav className="grow flex justify-evenly space-x-4">
-          <Link to="/" className="font-bold hover:text-gray-500 text-sm">
+        <nav className="grow flex justify-evenly ">
+          <Link to="/" className="font-bold hover:text-gray-200 text-sm">
             BERANDA
           </Link>
-          <Link to="/shop" className="font-bold hover:text-gray-500 text-sm">
+          <Link to="/shop" className="font-bold hover:text-gray-200 text-sm">
             PRODUK
           </Link>
           <Link
             to="/about-us"
-            className="font-bold hover:text-gray-500 text-sm"
+            className="font-bold hover:text-gray-200 text-sm"
           >
             TENTANG KAMI
           </Link>
         </nav>
-        <div className="flex justify-evenly space-x-4 w-1/5">
-          <Link to="/cart" className="text-white hover:text-gray-900 relative">
+        <div className="flex justify-evenly  w-1/6">
+          <Link to="/cart" className="text-white hover:text-gray-200 relative">
             {/* {cart.length < 1 ? (
               ""
             ) : (
@@ -46,15 +46,8 @@ export default function Header() {
             </div>
             <SlBag className="text-xl" />
           </Link>
-          {/* <Link to="/profile" className="text-white hover:text-gray-900 ">
+          <Link to="/profile" className="text-white hover:text-gray-200 ">
             <FiUser className="text-xl" />
-          </Link> */}
-
-          <Link
-            to="/"
-            className="bg-white hover:bg-gray-200 text-brown-dark font-bold py-1 px-4 rounded"
-          >
-            Sign In
           </Link>
         </div>
       </div>
