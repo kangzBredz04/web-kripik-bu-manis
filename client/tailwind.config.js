55d1888afead5d4b4cec87379485c14de7afc476
wahyu <wabredz1234@gmail.com>
2024-06-14 17:18:10 +0700

    menambahkan konfigurasi untuk warna dan font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brown-light": "#D2B48C",
+        orange: "#FFA500",
+        "green-leaf": "#8FBC8F",
+        "brown-dark": "#8B4513",
+        cream: "#FFF8DC",
+      },
+      fontFamily: {
+        pacifico: ["Pacifico", "cursive"],
+        roboto: ["Roboto", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
