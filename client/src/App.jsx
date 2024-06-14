db1907b82f0aab5468185a6f0c956ec132f4d410
wahyu <wabredz1234@gmail.com>
2024-06-15 00:46:56 +0700

    menambahkan logika untuk menampilkan produk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { api } from "./utils.js";
 
 export const AllContext = createContext();
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get("/product/get-all").then((response) => setProducts(response));
+  }, []);
   return (
-    <AllContext.Provider value={{}}>
+    <AllContext.Provider
+      value={{
+        products,
+        setProducts,
+      }}
+    >
       <Header />
       <Outlet />
       <Footer />
