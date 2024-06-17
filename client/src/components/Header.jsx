246d1f8c4507ad15170c86fe388b540ab5e0e147
wahyu <wabredz1234@gmail.com>
2024-06-17 15:37:26 +0700

    Mengubah tampilan header dan menambahkan pop up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { GoBookmark } from "react-icons/go";
 import { MdOutlineLanguage } from "react-icons/md";
 import { FiUser, FiMoon } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import { SlBag } from "react-icons/sl";
+import { useState } from "react";
 // import { useContext } from "react";
 // import { AllContext } from "../App";
 
 export default function Header() {
   // const { wishlist, cart } = useContext(AllContext);
+  const [showPopup, setShowPopup] = useState(false);
+  const navigate = useNavigate();
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
   return (
     <header className="bg-brown-dark text-white border  sticky top-0 z-50 font-poppins">
       <div className="container mx-auto  py-4 flex justify-between items-center">
@@ -33,22 +41,47 @@ export default function Header() {
         </nav>
         <div className="flex justify-evenly  w-1/6">
           <Link to="/cart" className="text-white hover:text-gray-200 relative">
-            {/* {cart.length < 1 ? (
-              ""
-            ) : (
-              <div className="absolute -right-2 -top-2 text-white px-1 bg-red-600 rounded-full text-xs">
-                {cart?.length}
-              </div>
-            )} */}
-
             <div className="absolute -right-2 -top-2 text-brown-dark px-[5px] bg-white rounded-full text-xs font-semibold">
               1
             </div>
             <SlBag className="text-xl" />
           </Link>
-          <Link to="/profile" className="text-white hover:text-gray-200 ">
-            <FiUser className="text-xl" />
-          </Link>
+          {localStorage.getItem("id") ? (
+            <Link
+              // to="/login"
+              onClick={togglePopup}
+              className="text-white hover:text-gray-200"
+            >
+              <FiUser className="text-xl" />
+            </Link>
+          ) : (
+            <Link
+              to={"/login"}
+              className="font-semibold tracking-wider cursor-pointer"
+            >
+              LOGIN
+            </Link>
+          )}
+          {showPopup && (
+            <div className="absolute right-1 mt-11 w-48 bg-white border border-gray-200 shadow-lg rounded-lg">
+              <IoClose
+                size={25}
+                onClick={togglePopup}
+                className="absolute text-brown-dark cursor-pointer right-1"
+              />
+              <div className="p-4 flex flex-col items-center">
+                <FaUserCircle size={50} className="text-brown-dark mb-4" />
+                <div className="text-center">
+                  <p className="text-sm text-brown-dark font-semibold">
+                    Customer Code: 12345
+                  </p>
+                  <p className="text-sm text-brown-dark font-semibold">
+                    Name: John Doe
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
