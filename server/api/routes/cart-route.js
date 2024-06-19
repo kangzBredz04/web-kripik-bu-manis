5a27fbbab371ebe61400f4b7d86e2c6e88da3d93
wahyu <wabredz1234@gmail.com>
2024-06-20 00:28:27 +0700

    Menambahkan route untuk menambahkan produk kedalam keranjang

--- server/api/routes/cart-route.js
+++ server/api/routes/cart-route.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth-middleware.js";
 import {
-  //   addCart,
+  getCartByIdCustomer,
+  addCart,
   //   deleteCart,
-  getCartByIdUser,
   //   updateCart,
 } from "../controllers/cart-controller.js";
 
 const router = express.Router();
 
-router.get("/get/:id", getCartByIdUser); // Router untuk mendapatkan keranjang by id user
-// router.post("/add", verifyToken, addCart); // Router untuk menambahkan data keranjang
+router.get("/get/:id", getCartByIdCustomer); // Router untuk mendapatkan keranjang by id customer
+router.post("/add", addCart); // Router untuk menambahkan data keranjang
 // router.put("/update/:id", verifyToken, updateCart); // Router untuk mengubah data keranjang berdasarkan id
 // router.delete("/delete/:id", verifyToken, deleteCart); // Router untuk menghapus data keranjang berdasarkan id
 
