48a288c770cb8cb331ee7126f8a6a508179d91cb
wahyu <wabredz1234@gmail.com>
2024-06-20 11:16:52 +0700

    Merubah tampilan halaman NotLogin

--- client/src/pages/NotLogin.jsx
+++ client/src/pages/NotLogin.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NotLogin() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-      <h1 className="text-4xl font-bold text-gray-800 mb-8">Oops!</h1>
-      <p className="text-lg text-gray-600 mb-4">
+    <div className="py-40 flex flex-col items-center justify-center bg-white font-poppins">
+      <h1 className="text-4xl font-extrabold text-brown-dark mb-8">Oops!</h1>
+      <p className="text-lg text-brown-dark font-medium mb-4">
         Anda belum login. Silakan login terlebih dahulu untuk melanjutkan.
       </p>
       <Link
         to="/login"
-        className="bg-black border border-black hover:bg-white hover:text-black text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+        className="bg-brown-dark border border-brown-dark hover:bg-white hover:text-brown-dark text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
       >
         Login Sekarang
       </Link>
