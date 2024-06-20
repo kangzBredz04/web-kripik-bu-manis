a2f8bbb8ac8d0e44c99721c00ea681de5719734a
wahyu <wabredz1234@gmail.com>
2024-06-20 14:54:03 +0700

    Menambahkan route untuk update dan hapus keranjang

--- server/api/routes/cart-route.js
+++ server/api/routes/cart-route.js
@@ -3,15 +3,15 @@ import { verifyToken } from "../middleware/auth-middleware.js";
 import {
   getCartByIdCustomer,
   addCart,
-  //   deleteCart,
-  //   updateCart,
+  updateCart,
+  deleteCart,
 } from "../controllers/cart-controller.js";
 
 const router = express.Router();
 
 router.get("/get/:id", getCartByIdCustomer); // Router untuk mendapatkan keranjang by id customer
 router.post("/add", addCart); // Router untuk menambahkan data keranjang
-// router.put("/update/:id", verifyToken, updateCart); // Router untuk mengubah data keranjang berdasarkan id
-// router.delete("/delete/:id", verifyToken, deleteCart); // Router untuk menghapus data keranjang berdasarkan id
+router.put("/update/:id", updateCart); // Router untuk mengubah data keranjang berdasarkan id
+router.delete("/delete/:id", deleteCart); // Router untuk menghapus data keranjang berdasarkan id
 
 export default router;
