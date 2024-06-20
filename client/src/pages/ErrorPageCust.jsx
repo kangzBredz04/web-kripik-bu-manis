dd6b7e375abfa99508be6092541a92f004b1ca06
wahyu <wabredz1234@gmail.com>
2024-06-20 12:35:19 +0700

    Merubah tampilan halaman error cust

--- client/src/pages/ErrorPageCust.jsx
+++ client/src/pages/ErrorPageCust.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 export default function ErrorPageACust() {
   return (
     <div className="h-screen flex flex-col justify-center items-center font-KumbhSans">
-      <h1 className="text-4xl font-bold text-black mb-4">404 Not Found</h1>
-      <p className="text-gray-600 mb-8">
+      <h1 className="text-4xl font-extrabold text-brown-dark mb-4">
+        404 Not Found
+      </h1>
+      <p className="text-brown-dark font-bold mb-8">
         Maaf, halaman yang Anda cari tidak ditemukan.
       </p>
       <Link
         to="/"
-        className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-brown-dark border text-white border-brown-dark hover:bg-white hover:text-brown-dark font-bold py-2 px-4 rounded"
       >
         Kembali ke Beranda
       </Link>
