6e290688fb68df702480cfa5486deb6997445879
wahyu <wabredz1234@gmail.com>
2024-06-23 23:59:06 +0700

    Merubah logika pada fungsi add sales

--- server/api/controllers/sale-controller.js
+++ server/api/controllers/sale-controller.js
@@ -11,8 +11,8 @@ export const addSalesCustomer = async (req, res) => {
     address,
   } = req.body;
   try {
-    do {
-      sales.forEach(async (sale) => {
+    if (sales?.length > 1) {
+      for (const sale of sales) {
         const { id, id_product, total_product } = sale;
         await pool.query(
           "INSERT INTO sales (id_customer, id_product, total_product, sub_total, discount, total_sale, type_of_payment, address) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
@@ -32,30 +32,27 @@ export const addSalesCustomer = async (req, res) => {
           "UPDATE products SET stock = (stock - $1) WHERE id = $2",
           [total_product, id_product]
         );
-      });
-    } while (!sales.length);
-
-    //   for (const sale of sales) {
-    //     const { id, id_product, total_product } = sale;
-    //     await pool.query(
-    //       "INSERT INTO sales (id_customer, id_product, total_product, sub_total, discount, total_sale, type_of_payment, address) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-    //       [
-    //         id_customer,
-    //         id_product,
-    //         total_product,
-    //         sub_total,
-    //         discount,
-    //         total_sale,
-    //         type_of_payment,
-    //         address,
-    //       ]
-    //     );
-    //     await pool.query("DELETE FROM carts WHERE id= $1", [id]);
-    //     await pool.query(
-    //       "UPDATE products SET stock = (stock - $1) WHERE id = $2",
-    //       [total_product, id_product]
-    //     );
-    //   }
+      }
+    } else {
+      await pool.query(
+        "INSERT INTO sales (id_customer, id_product, total_product, sub_total, discount, total_sale, type_of_payment, address) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+        [
+          id_customer,
+          sales[0]?.id_product,
+          sales[0]?.total_product,
+          sub_total,
+          discount,
+          total_sale,
+          type_of_payment,
+          address,
+        ]
+      );
+      await pool.query("DELETE FROM carts WHERE id= $1", [sales[0]?.id]);
+      await pool.query(
+        "UPDATE products SET stock = (stock - $1) WHERE id = $2",
+        [sales[0]?.total_product, sales[0]?.id_product]
+      );
+    }
     res.status(200).json({ msg: "Pesanan telah berhasil dibuat" });
   } catch (error) {
     console.log(error);
