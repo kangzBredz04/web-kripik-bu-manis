e50d9f2bf25cbbf7649be31096a2148a7a1e12ea
wahyu <wabredz1234@gmail.com>
2024-06-23 23:30:38 +0700

    Membuat fungsi untuk menghapus kode voucher

--- server/api/controllers/code-voucher.js
+++ server/api/controllers/code-voucher.js
@@ -5,11 +5,23 @@ export const getCodeVoucher = async (req, res) => {
   try {
     const result = await pool.query(
       `
-      SELECT * FROM voucher_codes WHERE customer_id = $1
+      SELECT * FROM voucher_codes WHERE id_customer = $1
     `,
       [req.params.id]
     );
     res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+// Controller untuk menghapus data kode voucher berdasarkan id
+export const deleteCodeVoucher = async (req, res) => {
+  try {
+    await pool.query("DELETE FROM voucher_codes WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.send("Kode voucher berhasil dihapus.");
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
