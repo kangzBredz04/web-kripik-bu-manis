cf411f0dc7f556f1718380ebcedaccecc4050956
wahyu <wabredz1234@gmail.com>
2024-06-24 23:58:48 +0700

    Merubah beberapa bagian rute

--- server/api/routes/auth-route.js
+++ server/api/routes/auth-route.js
@@ -1,25 +1,26 @@
 import express from "express";
-import { verifyToken } from "../middleware/auth-middleware.js";
 import {
   addAccountUser,
+  deleteAccountUser,
   getAllUser,
   getCurrentUser,
   loginAccountAdmin,
   loginAccountCustomer,
   logoutAccount,
   registerAccount,
-  updateUser,
+  updateAccountUser,
 } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
-router.get("/my-account", verifyToken, getCurrentUser);
+router.get("/my-account", getCurrentUser);
 router.post("/register", registerAccount);
 router.post("/login-customer", loginAccountCustomer);
 router.post("/login-admin", loginAccountAdmin);
 router.get("/logout", logoutAccount);
 router.get("/get-all-user", getAllUser);
-router.put("/update-user/:id", updateUser);
+router.put("/update-user/:id", updateAccountUser);
 router.post("/add-account-user", addAccountUser);
+router.delete("/delete-user/:id", deleteAccountUser);
 
 export default router;
