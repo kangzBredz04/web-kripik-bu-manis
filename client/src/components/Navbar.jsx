0cda78df212cc773e9a42a337bc430bc6f10281b
wahyu <wabredz1234@gmail.com>
2024-06-24 14:19:03 +0700

    Mengubah tampilan dan logika pada komponen navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,11 +8,13 @@ import { FiUser, FiMoon } from "react-icons/fi";
 export default function Navbar() {
   return (
     <nav
-      className={`sticky top-0 py-6 px-5  flex justify-between items-center bg-gray-200`}
+      className={`sticky top-0 py-6 px-5 font-poppins flex justify-between items-center bg-brown-dark text-white`}
     >
       <div className="flex items-center">
         {/* <img src="/logo.png" alt="Logo" className="h-8 mr-4" /> */}
-        <h1 className="text-xl font-bold tracking-widest">ADMIN</h1>
+        <h1 className="text-xl font-bold tracking-widest">{`HALO SELAMAT DATANG ${localStorage
+          .getItem("name")
+          .toLocaleUpperCase()}`}</h1>
       </div>
       <div className="flex justify-evenly space-x-4 w-1/5">
         <Link
