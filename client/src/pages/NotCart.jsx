516f7751eb420bc3d06da7bce2e103a9422d3781
wahyu <wabredz1234@gmail.com>
2024-06-24 14:48:52 +0700

    Merubah tampilan pada halaman ketika keranjang kosong

--- client/src/pages/NotCart.jsx
+++ client/src/pages/NotCart.jsx
@@ -5,15 +5,17 @@ export default function NotCart() {
   return (
     <div className="flex flex-col items-center py-4 gap-5">
       <div className="relative">
-        <div className="absolute -right-4 -top-2 text-white px-3 py-1 bg-black rounded-full text-5xl">
+        <div className="absolute -right-4 -top-2 text-white px-3 py-1 bg-brown-dark rounded-full text-5xl">
           0
         </div>
-        <SlBag className="text-9xl" />
+        <SlBag className="text-9xl text-brown-dark" />
       </div>
-      <h1 className="text-2xl font-semibold">Your Cart Is Currently Empty</h1>
+      <h1 className="text-2xl font-semibold text-brown-dark">
+        Your Cart Is Currently Empty
+      </h1>
       <Link
         to="/shop"
-        className="bg-black text-white hover:bg-white hover:text-black py-3 px-7 outline"
+        className="bg-brown-dark text-white hover:bg-transparent hover:text-brown-dark py-3 px-7 outline"
       >
         RETURN TO SHOP
       </Link>
