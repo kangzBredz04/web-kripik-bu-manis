cae6ed8beb07bb760f954eb5827427898758f63c
wahyu <wabredz1234@gmail.com>
2024-06-24 22:05:56 +0700

    Menambahkan fungsi untuk mendapatkan semua user

--- server/api/controllers/auth-controller.js
+++ server/api/controllers/auth-controller.js
@@ -118,3 +118,12 @@ export const logoutAccount = async (_req, res) => {
   res.clearCookie("token");
   res.status(200).json({ msg: "Logout berhasil" });
 };
+
+export const getAllUser = async (_req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
