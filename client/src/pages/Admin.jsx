b324fd4da27b4835951dc6d42c558643bfc737fe
wahyu <wabredz1234@gmail.com>
2024-06-24 11:50:33 +0700

    Memperbaharui bagian admin

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,70 +1,17 @@
 import { Outlet } from "react-router-dom";
-import SideBar from "../components/SideBar";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { createContext, useEffect, useState } from "react";
-import { api } from "../utils";
+import SideBar from "../components/SideBar.jsx";
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import { createContext } from "react";
+import UnauthorizedPage from "./UnauthorizedPage.jsx";
 // import UnauthorizedPage from "./UnauthorizedPage";
 
 export const AdminContext = createContext();
 
 export default function Admin() {
-  const [products, setProducts] = useState();
-  const [user, setUser] = useState();
-  const [loading, setLoading] = useState(true);
-  const [popUp, setPopUp] = useState(false);
-  const [popUp2, setPopUp2] = useState(false);
-  const [editedProduct, setEditedProduct] = useState();
-  const [editedUser, setEditedUser] = useState();
-  const [editedStock, setEditedStock] = useState();
-  const [editedStatus, setEditedStatus] = useState();
-  const [editedSize, setEditedSize] = useState();
-  const [admin, setAdmin] = useState({});
-  const [idAdmin, setIdAdmin] = useState(0);
-  const [stocks, setStocks] = useState([]);
-  const [sizes, setSizes] = useState([]);
-  const [orders, setOrders] = useState([]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      api.get("/product/get-all").then((res) => setProducts(res));
-      setLoading(false);
-    }, 500);
-  }, [products?.id]);
-
-
-  if (localStorage.getItem("role") == "admin") {
+  if (localStorage.getItem("role") == "Super Admin") {
     return (
-      <AdminContext.Provider
-        value={{
-          products,
-          setProducts,
-          popUp,
-          setPopUp,
-          popUp2,
-          setPopUp2,
-          editedProduct,
-          setEditedProduct,
-          loading,
-          setLoading,
-          user,
-          setUser,
-          editedUser,
-          setEditedUser,
-          stocks,
-          setStocks,
-          editedStock,
-          setEditedStock,
-          sizes,
-          setSizes,
-          editedSize,
-          setEditedSize,
-          orders,
-          setOrders,
-          editedStatus,
-          setEditedStatus,
-        }}
-      >
+      <AdminContext.Provider value={{}}>
         <div className="flex h-screen overflow-hidden font-KumbhSans  bg-white text-black">
           {/* Sidebar */}
           <SideBar />
@@ -73,13 +20,13 @@ export default function Admin() {
             {/* Navbar */}
             <Navbar />
             {/* Main Content */}
-            <Outlet context={[admin, setAdmin, idAdmin]} />
+            <Outlet />
             <Footer />
           </div>
         </div>
       </AdminContext.Provider>
     );
   } else {
-    // return <UnauthorizedPage />;
+    return <UnauthorizedPage />;
   }
 }
