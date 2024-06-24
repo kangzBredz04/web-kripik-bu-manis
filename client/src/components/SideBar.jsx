b823662e2a430bd8a425b8b9024057e23ba8b192
wahyu <wabredz1234@gmail.com>
2024-06-24 14:19:25 +0700

    Mengubah tampilan komponen sidebar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -14,11 +14,11 @@ export default function SideBar() {
 
   return (
     <div
-      className={`font-KumbhSans flex flex-col gap-7  h-screen w-36 py-4 px-4 bg-gray-200`}
+      className={`font-poppins flex flex-col gap-7  h-screen w-40 py-4 px-4 bg-brown-dark text-white`}
     >
       <div className=" text-center">
-        <h1 className="text-xl font-bold tracking-widest">SAHABA</h1>
-        <h1 className="text-xl font-bold tracking-widest">FASHION</h1>
+        <h1 className="text-xl font-bold tracking-widest">KRIPIK</h1>
+        <h1 className="text-xl font-bold tracking-widest">BU MANIS</h1>
       </div>
       <div className="flex flex-col justify-between h-full">
         <Link className="text-center flex flex-col gap-1 hover:cursor-pointer">
