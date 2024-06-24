a12a6cded9b0fde932ee315e467760734f2975db
wahyu <wabredz1234@gmail.com>
2024-06-24 22:37:09 +0700

    Membuat rute untuk mengupdate data user

--- server/api/routes/auth-route.js
+++ server/api/routes/auth-route.js
@@ -1,19 +1,23 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth-middleware.js";
 import {
+  getAllUser,
   getCurrentUser,
   loginAccountAdmin,
   loginAccountCustomer,
   logoutAccount,
   registerAccount,
+  updateUser,
 } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
-router.get("/my-account", verifyToken, getCurrentUser); // Router untuk mendapatkan data user yang sedang login
-router.post("/register", registerAccount); // Router untuk registrasi akun
-router.post("/login-customer", loginAccountCustomer); // Router untuk login akun
-router.post("/login-admin", loginAccountAdmin); // Router untuk login akun
-router.get("/logout", logoutAccount); // Router untuk logout account
+router.get("/my-account", verifyToken, getCurrentUser);
+router.post("/register", registerAccount);
+router.post("/login-customer", loginAccountCustomer);
+router.post("/login-admin", loginAccountAdmin);
+router.get("/logout", logoutAccount);
+router.get("/get-all-user", getAllUser);
+router.put("/update-user/:id", updateUser);
 
 export default router;
