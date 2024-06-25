0234ed9aea74bf2ee19b50c609bd7c937776ad62
wahyu <wabredz1234@gmail.com>
2024-06-25 23:57:20 +0700

    Menabahkan ikon dan rute untuk mengakses halaman kasir

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,17 +1,11 @@
 import { MdOutlineDashboard } from "react-icons/md";
-// import { GrLogout } from "react-icons/gr";
 import { FiUsers } from "react-icons/fi";
-import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 import { LuBoxes } from "react-icons/lu";
-import { RiShirtLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
-// import { useContext } from "react";
-// import { AdminContext } from "../pages/Admin";
-// import { api } from "../utils";
+import { TiShoppingCart } from "react-icons/ti";
+import { FaCashRegister } from "react-icons/fa";
 
 export default function SideBar() {
-  // const { theme } = useContext(AdminContext);
-
   return (
     <div
       className={`font-poppins flex flex-col gap-7  h-screen w-40 py-4 px-4 bg-brown-dark text-white`}
@@ -36,22 +30,22 @@ export default function SideBar() {
           to="/admin/product"
           className="text-center flex flex-col gap-1 hover:cursor-pointer"
         >
-          <RiShirtLine className="m-auto text-3xl" />
+          <LuBoxes className="m-auto text-3xl" />
           <h1 className="text-base font-medium">Produk</h1>
         </Link>
         <Link
           to="/admin/order"
           className="text-center flex flex-col gap-1 hover:cursor-pointer"
         >
-          <LiaFileInvoiceDollarSolid className="m-auto text-3xl" />
+          <TiShoppingCart className="m-auto text-3xl" />
           <h1 className="text-base font-medium">Pesanan</h1>
         </Link>
         <Link
-          to="/admin/stock"
+          to="/admin/cashier"
           className="text-center flex flex-col gap-1 hover:cursor-pointer"
         >
-          <LuBoxes className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Stok</h1>
+          <FaCashRegister className="m-auto text-3xl" />
+          <h1 className="text-base font-medium">Kasir</h1>
         </Link>
       </div>
     </div>
