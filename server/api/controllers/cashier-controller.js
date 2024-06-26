8d8b8d27f0c6d3f26a0e9da5da1dd2dfdafaaf87
wahyu <wabredz1234@gmail.com>
2024-06-26 23:17:45 +0700

    menambah bebarapa fungsi

--- server/api/controllers/cashier-controller.js
+++ server/api/controllers/cashier-controller.js
@@ -13,3 +13,36 @@ export const addCashierProduct = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getAllProductCashier = async (_req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT c.id, c.total_product, c.price, c.sub_total, p.name
+      FROM cashier_product c
+      JOIN products p ON c.id_product = p.id`
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const deleteProductCashier = async (req, res) => {
+  try {
+    await pool.query("DELETE FROM cashier_product WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.status(200).json({ msg: "Success" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const deleteAllProductCashier = async (req, res) => {
+  try {
+    await pool.query("DELETE FROM cashier_product");
+    res.status(200).json({ msg: "Success" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
