361631143425604e13241d15868c4f55a1d23861
wahyu <wabredz1234@gmail.com>
2024-06-26 09:25:12 +0700

    Megupdare halaman kasir

--- client/src/pages/Cashier.jsx
+++ client/src/pages/Cashier.jsx
@@ -5,9 +5,9 @@ import CashierCardProduct from "../components/CashierCardProduct";
 export default function Cashier() {
   const { products } = useContext(AdminContext);
   return (
-    <div className="flex">
+    <div className="bg-gray-200 flex">
       <div className="w-2/3">
-        <div className="grid grid-cols-4 gap-5 px-5 py-5">
+        <div className="grid grid-cols-4 gap-5 px-4 py-4">
           {products?.map((p) => (
             <CashierCardProduct
               key={p.id}
@@ -21,7 +21,22 @@ export default function Cashier() {
           ))}
         </div>
       </div>
-      <div className="w-1/3">Sisi Kiri</div>
+      <div className="w-1/3 m-5 px-4 py-2 border border-black">
+        <div className="flex flex-col items-center px-2 py-3">
+          <h1>Checkout</h1>
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          <p className=" text-center text-xs font-extrabold tracking-wider">
+            PRODUK
+          </p>
+          <p className=" text-center text-xs font-extrabold tracking-wider">
+            JUMLAH PRODUK
+          </p>
+          <p className=" text-center text-xs font-extrabold tracking-wider">
+            SUBTOTAL
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
