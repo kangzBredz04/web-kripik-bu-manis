b54e763739dfa81199885b6d947f366cac6f2cff
wahyu <wabredz1234@gmail.com>
2024-06-26 23:12:15 +0700

    merubah logika dan tampilan komponen kartu produk kasir

--- client/src/components/CashierCardProduct.jsx
+++ client/src/components/CashierCardProduct.jsx
@@ -1,24 +1,37 @@
+/* eslint-disable no-undef */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 
-export default function CashierCardProduct({
-  id,
-  name,
-  image,
-  description,
-  price,
-  stock,
-}) {
+import { useEffect, useState } from "react";
+import { api } from "../utils";
+
+/* eslint-disable react/prop-types */
+export default function CashierCardProduct({ id, name, image, price }) {
+  const [cashierProduct, setCashierProduct] = useState({});
+
+  useEffect(() => {
+    setCashierProduct({
+      id_product: id,
+      total_product: 1,
+      price: price,
+    });
+  }, []);
+
   return (
-    <div className="bg-white cursor-pointer flex flex-col justify-between font-poppins rounded-md">
+    <div
+      onClick={() => {
+        api.post("/cashier/add", cashierProduct).then((res) => {
+          window.location.reload();
+        });
+      }}
+      className="bg-white cursor-pointer flex flex-col justify-between font-poppins rounded-md"
+    >
       <div className="rounded-md">
         <img src={image} alt="" className="rounded-md" />
         <div className="p-1 items-center flex flex-col justify-between">
           <div className="flex flex-col gap-1">
-            <p className="text-md text-center text-brown-dark font-bold">
-              {name}
-            </p>
-            <p className="text-base font-bold text-center">
+            <p className="text-base text-center font-bold">{name}</p>
+            <p className="text-base font-extrabold text-center text-teal">
               Rp{parseInt(price).toLocaleString("id-ID")}
             </p>
           </div>
