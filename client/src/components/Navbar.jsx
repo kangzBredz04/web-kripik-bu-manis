010569774da522e599cc789c81e3d07be4e23396
wahyu <wabredz1234@gmail.com>
2024-06-26 09:24:34 +0700

    Memperbaharui tampilan komponen navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,15 @@ import { FiUser } from "react-icons/fi";
 
 export default function Navbar() {
   return (
-    <nav
-      className={`sticky top-0 py-6 px-5 font-poppins flex justify-between items-center bg-brown-dark text-white`}
-    >
-      <div className="flex items-center">
+    <nav className="sticky top-0 py-6 px-5 font-poppins flex justify-between items-center bg-brown-dark text-white">
+      <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-widest">{`HALO SELAMAT DATANG ${localStorage
           .getItem("name")
           .toLocaleUpperCase()}`}</h1>
       </div>
-      <div className="flex justify-evenly space-x-4 w-1/5">
-        <Link to="/admin/profile">
+      <div className="">
+        <Link to="/admin/profile" className="flex items-center gap-3">
+          <p>Profile</p>
           <FiUser className="text-2xl" />
         </Link>
       </div>
