504e087848b73cdf7103712ab95f208b1c9b8fc8
wahyu <wabredz1234@gmail.com>
2024-06-26 09:24:20 +0700

    Memperbaharui tampilan halaman kasir admin

--- client/src/components/CashierCardProduct.jsx
+++ client/src/components/CashierCardProduct.jsx
@@ -1,34 +1,26 @@
-export default function CashierCardProduct(
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+export default function CashierCardProduct({
   id,
   name,
   image,
   description,
-  price
-) {
+  price,
+  stock,
+}) {
   return (
-    <div className="bg-white flex flex-col justify-between font-poppins rounded-md">
+    <div className="bg-white cursor-pointer flex flex-col justify-between font-poppins rounded-md">
       <div className="rounded-md">
         <img src={image} alt="" className="rounded-md" />
-        <div className="p-5 flex flex-col justify-between h-52">
+        <div className="p-1 items-center flex flex-col justify-between">
           <div className="flex flex-col gap-1">
-            <p className="text-xl text-brown-dark font-bold">{name}</p>
-            <p className="text-base font-bold">
+            <p className="text-md text-center text-brown-dark font-bold">
+              {name}
+            </p>
+            <p className="text-base font-bold text-center">
               Rp{parseInt(price).toLocaleString("id-ID")}
             </p>
-            <p className="text-xs font-medium">{description}</p>
-          </div>
-          <div>
-            <button
-              className="py-2 px-5 bg-brown-dark rounded-3xl text-white text-xs font-bold"
-              onClick={() => {
-                localStorage.setItem("id_product", id);
-                window.location.href = `/product/${localStorage.getItem(
-                  "id_product"
-                )}`;
-              }}
-            >
-              PESAN SEKARANG
-            </button>
           </div>
         </div>
       </div>
