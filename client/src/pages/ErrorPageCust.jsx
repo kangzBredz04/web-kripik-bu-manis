8f16626a6204ea4f21bc5884c2f23a44d452d567
wahyu <wabredz1234@gmail.com>
2024-06-27 16:24:08 +0700

    mengupdate warna dan backgorund halaman error

--- client/src/pages/ErrorPageCust.jsx
+++ client/src/pages/ErrorPageCust.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 export default function ErrorPageACust() {
   return (
     <div className="h-screen flex flex-col justify-center items-center font-KumbhSans">
-      <h1 className="text-4xl font-extrabold text-brown-dark mb-4">
-        404 Not Found
-      </h1>
-      <p className="text-brown-dark font-bold mb-8">
+      <h1 className="text-4xl font-extrabold text-teal mb-4">404 Not Found</h1>
+      <p className="text-teal font-bold mb-8">
         Maaf, halaman yang Anda cari tidak ditemukan.
       </p>
       <Link
         to="/"
-        className="bg-brown-dark border text-white border-brown-dark hover:bg-white hover:text-brown-dark font-bold py-2 px-4 rounded"
+        className="bg-teal border text-white border-teal hover:bg-white hover:text-teal font-bold py-2 px-4 rounded"
       >
         Kembali ke Beranda
       </Link>
