6098661b1eb15befe8b3234a4da92a7ca987e2ad
wahyu <wabredz1234@gmail.com>
2024-06-27 16:51:43 +0700

    mengupdate tampilan sesuai role

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -12,40 +12,63 @@ export default function SideBar() {
         <h1 className="text-xl font-bold tracking-widest">KRIPIK</h1>
         <h1 className="text-xl font-bold tracking-widest">BU MANIS</h1>
       </div>
-      <div className="flex flex-col justify-between h-full">
-        <Link className="text-center flex flex-col gap-1 hover:cursor-pointer">
-          <MdOutlineDashboard className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Beranda</h1>
-        </Link>
-        <Link
-          to="/admin/customer-user"
-          className="text-center flex flex-col gap-1 hover:cursor-pointer"
-        >
-          <FiUsers className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Pengguna</h1>
-        </Link>
-        <Link
-          to="/admin/product"
-          className="text-center flex flex-col gap-1 hover:cursor-pointer"
-        >
-          <LuBoxes className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Produk</h1>
-        </Link>
-        <Link
-          to="/admin/sales-report"
-          className="text-center flex flex-col gap-1 hover:cursor-pointer"
-        >
-          <TiShoppingCart className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Penjualan</h1>
-        </Link>
-        <Link
-          to="/admin/cashier"
-          className="text-center flex flex-col gap-1 hover:cursor-pointer"
-        >
-          <FaCashRegister className="m-auto text-3xl" />
-          <h1 className="text-base font-medium">Kasir</h1>
-        </Link>
-      </div>
+      {localStorage.getItem("role") == "Super Admin" ? (
+        <div className="flex flex-col justify-around h-full">
+          <Link className="text-center flex flex-col gap-1 hover:cursor-pointer">
+            <MdOutlineDashboard className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Beranda</h1>
+          </Link>
+          <Link
+            to="/admin/customer-user"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <FiUsers className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Pengguna</h1>
+          </Link>
+          <Link
+            to="/admin/sales-report"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <TiShoppingCart className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Penjualan</h1>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-between h-full">
+          <Link className="text-center flex flex-col gap-1 hover:cursor-pointer">
+            <MdOutlineDashboard className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Beranda</h1>
+          </Link>
+          <Link
+            to="/admin/customer-user"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <FiUsers className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Pengguna</h1>
+          </Link>
+          <Link
+            to="/admin/product"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <LuBoxes className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Produk</h1>
+          </Link>
+          <Link
+            to="/admin/sales-report"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <TiShoppingCart className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Penjualan</h1>
+          </Link>
+          <Link
+            to="/admin/cashier"
+            className="text-center flex flex-col gap-1 hover:cursor-pointer"
+          >
+            <FaCashRegister className="m-auto text-3xl" />
+            <h1 className="text-base font-medium">Kasir</h1>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
