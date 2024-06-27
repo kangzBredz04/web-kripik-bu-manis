9f7318154d0ee94abcee3379e10b98f342d2d1e3
wahyu <wabredz1234@gmail.com>
2024-06-27 16:24:42 +0700

    mengupdate warna dan backgorund halaman apabila keranjang kosong

--- client/src/pages/NotCart.jsx
+++ client/src/pages/NotCart.jsx
@@ -5,17 +5,17 @@ export default function NotCart() {
   return (
     <div className="flex flex-col items-center py-4 gap-5">
       <div className="relative">
-        <div className="absolute -right-4 -top-2 text-white px-3 py-1 bg-brown-dark rounded-full text-5xl">
+        <div className="absolute -right-4 -top-2 text-white px-3 py-1 bg-teal rounded-full text-5xl">
           0
         </div>
-        <SlBag className="text-9xl text-brown-dark" />
+        <SlBag className="text-9xl text-teal" />
       </div>
-      <h1 className="text-2xl font-semibold text-brown-dark">
+      <h1 className="text-2xl font-semibold text-teal">
         Your Cart Is Currently Empty
       </h1>
       <Link
         to="/shop"
-        className="bg-brown-dark text-white hover:bg-transparent hover:text-brown-dark py-3 px-7 outline"
+        className="bg-teal text-white hover:bg-transparent hover:text-teal py-3 px-7 outline"
       >
         RETURN TO SHOP
       </Link>
