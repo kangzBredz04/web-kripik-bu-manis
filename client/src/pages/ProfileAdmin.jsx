d59850b9a4787144759226403fd9cc026fe00761
wahyu <wabredz1234@gmail.com>
2024-06-27 16:25:13 +0700

    mengupdate warna dan backgorund halaman profile admin

--- client/src/pages/ProfileAdmin.jsx
+++ client/src/pages/ProfileAdmin.jsx
@@ -7,17 +7,12 @@ export default function ProfileAdmin() {
 
   if (localStorage.getItem("id")) {
     return (
-      <div className="py-6 px-7 font-KumbhSans bg-gray-100">
-        <h2 className="text-2xl font-extrabold mb-4 text-brown-dark">
-          My Account
-        </h2>
+      <div className="py-6 px-7 font-KumbhSans bg-warm-gray">
+        <h2 className="text-2xl font-extrabold mb-4">My Account</h2>
         <form>
           <div className="flex gap-5">
             <div className="grow mb-4">
-              <label
-                htmlFor="name"
-                className="block text-brown-dark font-extrabold mb-2"
-              >
+              <label htmlFor="name" className="block font-extrabold mb-2">
                 Name
               </label>
               <input
@@ -26,14 +21,11 @@ export default function ProfileAdmin() {
                 name="name"
                 value={localStorage.getItem("name")}
                 disabled
-                className="w-full border-2 text-brown-dark font-bold border-brown-dark px-2 py-2 focus:outline-none focus:border-gray-600"
+                className="w-full border-2  font-bold border-teal px-2 py-2 focus:outline-none focus:border-gray-600"
               />
             </div>
             <div className="grow mb-4">
-              <label
-                htmlFor="role"
-                className="block text-brown-dark font-extrabold mb-2"
-              >
+              <label htmlFor="role" className="block  font-extrabold mb-2">
                 Role
               </label>
               <input
@@ -42,16 +34,13 @@ export default function ProfileAdmin() {
                 name="role"
                 value={localStorage.getItem("role")}
                 disabled
-                className="w-full border-2 text-brown-dark font-bold border-brown-dark px-2 py-2 focus:outline-none focus:border-gray-600"
+                className="w-full border-2 font-bold border-teal px-2 py-2 focus:outline-none focus:border-gray-600"
               />
             </div>
           </div>
           {/* Username */}
           <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-brown-dark font-extrabold mb-2"
-            >
+            <label htmlFor="username" className="block font-extrabold mb-2">
               Username
             </label>
             <input
@@ -60,15 +49,12 @@ export default function ProfileAdmin() {
               name="username"
               value={localStorage.getItem("username")}
               disabled
-              className="w-full border-2 text-brown-dark font-bold border-brown-dark px-2 py-2 focus:outline-none focus:border-gray-600"
+              className="w-full border-2 font-bold border-teal px-2 py-2 focus:outline-none focus:border-gray-600"
             />
           </div>
           {/* Email */}
           <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-brown-dark font-extrabold mb-2"
-            >
+            <label htmlFor="email" className="block font-extrabold mb-2">
               Password
             </label>
             <input
@@ -77,34 +63,12 @@ export default function ProfileAdmin() {
               name="email"
               value="* * * * * *"
               disabled
-              className="w-full border-2 text-brown-dark font-bold border-brown-dark px-2 py-2 focus:outline-none focus:border-gray-600"
+              className="w-full border-2 font-bold border-teal px-2 py-2 focus:outline-none focus:border-gray-600"
             />
           </div>
         </form>
 
         <div className="w-full flex mt-5 justify-end gap-3">
-          {localStorage.getItem("id") != 1 && (
-            <button
-              className="w-1/3 bg-brown-dark text-white font-bold tracking-widest py-3 hover:bg-white hover:text-black outline"
-              onClick={() => {
-                if (confirm("Apakah yakin anda akan menghapus akun anda ?")) {
-                  api
-                    .delete(`/auth/delete/${localStorage.getItem("id")}`)
-                    .then((res) => {
-                      alert(res.msg);
-                      window.location.reload();
-                      //   setUser();
-                      localStorage.removeItem("token");
-                      localStorage.removeItem("role");
-                      localStorage.removeItem("id");
-                      navigate("/login");
-                    });
-                }
-              }}
-            >
-              Delete Account
-            </button>
-          )}
           <button
             onClick={() => {
               if (confirm("Apakah yakin anda akan logout")) {
@@ -117,7 +81,7 @@ export default function ProfileAdmin() {
                 });
               }
             }}
-            className="w-1/3 bg-brown-dark text-white font-bold  py-3 hover:bg-white hover:text-brown-dark outline"
+            className="w-1/3 bg-teal text-white font-bold  py-3 hover:bg-transparent hover:border-2 hover:border-teal hover:text-teal"
           >
             Logout
           </button>
