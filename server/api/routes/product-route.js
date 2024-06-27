8d31a9890ec2be10901c6c4e1b8f0299aea70845
wahyu <wabredz1234@gmail.com>
2024-06-27 17:44:43 +0700

    tambah rute untuk update data produk

--- server/api/routes/product-route.js
+++ server/api/routes/product-route.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   addProduct,
   getAllProduct,
+  updateProduct,
 } from "../controllers/product-controller.js";
 
 const router = express.Router();
 
 router.get("/get-all", getAllProduct);
 router.post("/add", addProduct);
+router.put("/update/:id", updateProduct);
 export default router;
