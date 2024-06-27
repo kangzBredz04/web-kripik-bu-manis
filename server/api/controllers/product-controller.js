9fbf607b9974014a6a7fcc4bc44c3e0878bacbfc
wahyu <wabredz1234@gmail.com>
2024-06-27 17:40:27 +0700

    menambahkan fungsi untuk menambah produk

--- server/api/controllers/product-controller.js
+++ server/api/controllers/product-controller.js
@@ -9,3 +9,24 @@ export const getAllProduct = async (_req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+export const addProduct = async (req, res) => {
+  try {
+    const result = await pool.query(
+      "INSERT INTO products (name,  description, price, stock, image) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        req.body.name,
+        req.body.description,
+        req.body.price,
+        req.body.stock,
+        req.body.image,
+      ]
+    );
+    res.json({
+      product: result.rows[0],
+      message: "Produk berhasil ditambahkan.",
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
