3b884efd7c997619c8fbb9f285765fdadff6e9b0
wahyu <wabredz1234@gmail.com>
2024-06-28 21:40:35 +0700

    last commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,60 +1,82 @@
-/* eslint-disable no-unused-vars */
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import CardProduct from "../components/CardProduct";
 import { AllContext } from "../App";
-
-// const products = [
-//   {
-//     id: 1,
-//     name: "Keripik Original",
-//     price: 20000,
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modimagni quibusdam dolor cumque",
-//     image: "/Keripik Original.jpeg",
-//   },
-//   {
-//     id: 2,
-//     name: "Keripik Pedas",
-//     price: 10000,
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modimagni quibusdam dolor cumque",
-//     image: "/Keripik Pedas.jpeg",
-//   },
-//   {
-//     id: 3,
-//     name: "Keripik Coklat",
-//     price: 15000,
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modimagni quibusdam dolor cumque",
-//     image: "/Keripik Coklat.jpeg",
-//   },
-//   {
-//     id: 1,
-//     name: "Keripik Original",
-//     price: 20000,
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modimagni quibusdam dolor cumque",
-//     image: "/Keripik Original.jpeg",
-//   },
-// ];
+import { Link } from "react-router-dom";
+import { FaUserLarge } from "react-icons/fa6";
 
 export default function Home() {
   const { products } = useContext(AllContext);
 
+  const testimonials = [
+    {
+      id: 1,
+      name: "Andi",
+      location: "Jakarta",
+      message:
+        "Awalnya saya ragu coba keripik pisang, tapi setelah gigitan pertama, langsung jatuh cinta! Rasanya manis alami, gak terlalu over. Anak-anak saya juga suka banget. Keripik Bu Manis jadi camilan favorit di rumah.",
+    },
+    {
+      id: 2,
+      name: "Siti",
+      location: "Bandung",
+      message:
+        "Saya penggemar berat keripik tempe pedas Bu Manis. Pedasnya pas, gak bikin sakit perut tapi tetap bikin nagih. Teman-teman di kantor juga banyak yang suka setelah saya bawa untuk dicoba. Top banget!",
+    },
+    {
+      id: 3,
+      name: "Rahmat",
+      location: "Medan",
+      message:
+        "Sebagai penikmat camilan pedas, keripik tempe pedas Bu Manis sangat memuaskan. Kualitas produknya luar biasa dan rasanya tidak mengecewakan. Sering saya jadikan oleh-oleh untuk keluarga di kampung.",
+    },
+  ];
+
   const filteredProducts = products.slice(1, 5);
 
   return (
-    <div className="flex flex-col gap-20 bg-warm-gray">
-      {/* <div className="w-full"></div> */}
-      <div className="flex flex-col">
-        <div className="flex flex-col gap-3 py-8 m-auto">
+    <div className="flex flex-col gap-16 bg-warm-gray font-poppins">
+      <div className="px-24 py-10 flex flex-row gap-11 bg-teal text-white">
+        <div className="flex flex-col py-3 gap-7 w-3/5">
           <div>
-            <h1 className="text-center font-medium text-xl tracking-wider">
+            <p className="text-6xl font-extrabold tracking-widest font-caveat">
+              Kripik Bu Manis
+            </p>
+          </div>
+          <p className="text-base tracking-normal">
+            Kripik Bu Manis adalah pilihan tepat untuk pecinta camilan gurih dan
+            renyah. Kami menggunakan bahan-bahan alami berkualitas tinggi untuk
+            memastikan setiap gigitan memberikan pengalaman rasa yang memuaskan.
+            Dibuat dengan resep tradisional yang diwariskan secara
+            turun-temurun, keripik kami menawarkan kombinasi sempurna antara
+            rasa dan tekstur.
+          </p>
+          <Link to={"/shop"} className=" w-1/2">
+            <button className="bg-white text-teal w-full py-3 font-bold tracking-wide rounded-md  hover:bg-transparent hover:text-white hover:outline hover:outline-white">
+              Belanja Sekarang
+            </button>
+          </Link>
+        </div>
+        <div className="w-2/5">
+          <img
+            src="kripik singkong balado.jpg"
+            alt=""
+            className="w-full rounded-s-[40px]"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-3 m-auto">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-center text-teal font-extrabold text-3xl tracking-wider">
               BEST SELLER
             </h1>
+            <p className="text-center text-teal font-semibold mb-6">
+              Temukan produk-produk terbaik kami yang paling laris di pasaran.
+              Dijamin enak dan berkualitas tinggi!
+            </p>
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-5 px-5 py-5">
+        <div className="grid grid-cols-4 gap-5 px-5 py-2">
           {filteredProducts.map((p) => (
             <CardProduct
               key={p.id}
@@ -65,6 +87,35 @@ export default function Home() {
               description={p.description}
               stock={p.stock}
             />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-3 m-auto">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-center text-teal font-extrabold text-3xl tracking-wider">
+              TESTIMONI
+            </h1>
+            <p className="text-center text-teal font-semibold mb-6">
+              Kepuasan pelanggan adalah prioritas utama kami
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 gap-10 px-5 py-2 mb-16">
+          {testimonials.map((t) => (
+            <div
+              key={t.id}
+              className="flex flex-col rounded-lg items-center px-4 py-4 bg-teal text-white"
+            >
+              <div className="relative p-5 rounded-full -top-5 outline bg-white text-teal">
+                <FaUserLarge size={25} />
+              </div>
+              <div className="flex flex-col items-center mb-5">
+                <p className="text-base font-extrabold">{t.name}</p>
+                <p className="text-sm">{t.location}</p>
+              </div>
+              <p className="text-center text-sm">{`" ${t.message} "`}</p>
+            </div>
           ))}
         </div>
       </div>
