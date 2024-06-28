ff6d59cf87e74038dbc059ae803805da5dd3d457
wahyu <wabredz1234@gmail.com>
2024-06-28 21:41:11 +0700

    last commit

--- server/api/controllers/sale-controller.js
+++ server/api/controllers/sale-controller.js
@@ -11,35 +11,14 @@ export const addSalesCustomer = async (req, res) => {
     address,
   } = req.body;
   try {
-    if (sales?.length > 1) {
-      for (const sale of sales) {
-        const { id, id_product, total_product } = sale;
-        await pool.query(
-          "INSERT INTO sales (id_customer, id_product, total_product, sub_total, discount, total_sale, type_of_payment, address) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-          [
-            id_customer,
-            id_product,
-            total_product,
-            sub_total,
-            discount,
-            total_sale,
-            type_of_payment,
-            address,
-          ]
-        );
-        await pool.query("DELETE FROM carts WHERE id= $1", [id]);
-        await pool.query(
-          "UPDATE products SET stock = (stock - $1) WHERE id = $2",
-          [total_product, id_product]
-        );
-      }
-    } else {
+    for (const sale of sales) {
+      const { id, id_product, total_product } = sale;
       await pool.query(
         "INSERT INTO sales (id_customer, id_product, total_product, sub_total, discount, total_sale, type_of_payment, address) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
         [
           id_customer,
-          sales[0]?.id_product,
-          sales[0]?.total_product,
+          id_product,
+          total_product,
           sub_total,
           discount,
           total_sale,
@@ -47,10 +26,26 @@ export const addSalesCustomer = async (req, res) => {
           address,
         ]
       );
-      await pool.query("DELETE FROM carts WHERE id= $1", [sales[0]?.id]);
+
+      const totalQuantityResult = await pool.query(
+        "SELECT SUM(total_product) AS total FROM sales WHERE id_customer = $1",
+        [id_customer]
+      );
+
+      const totalQuantity = totalQuantityResult.rows[0].total;
+
+      if (totalQuantity >= 1000) {
+        await pool.query(
+          "INSERT INTO voucher_codes (id_customer, code, discount) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
+          [id_customer, "DISKON30", 30]
+        );
+      }
+
+      await pool.query("DELETE FROM carts WHERE id = $1", [id]);
+
       await pool.query(
         "UPDATE products SET stock = (stock - $1) WHERE id = $2",
-        [sales[0]?.total_product, sales[0]?.id_product]
+        [total_product, id_product]
       );
     }
     res.status(200).json({ msg: "Pesanan telah berhasil dibuat" });
@@ -79,6 +74,36 @@ export const getBestProduct = async (_req, res) => {
       `SELECT p.id, p.name, p.price, p.image, SUM(s.total_product) as total_sales
       FROM products p JOIN sales s ON p.id = s.id_product
       GROUP BY p.id, p.name ORDER BY total_sales DESC LIMIT 3;`
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getBestCustomer = async (_req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT c.id, c.name, SUM(s.total_product) as total_sales
+      FROM customers c JOIN sales s ON c.id = s.id_customer WHERE c.id != 1
+      GROUP BY c.id, c.name ORDER BY total_sales DESC LIMIT 3;`
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+export const getSaleByIdCustomer = async (req, res) => {
+  try {
+    const result = await pool.query(
+      `
+      SELECT s.id, s.total_product, s.type_of_payment, p.name, p.image, p.price
+      FROM sales s
+      JOIN customers c ON s.id_customer = c.id
+      JOIN products p ON s.id_product = p.id WHERE s.id_customer = $1
+    `,
+      [req.params.id]
     );
     res.status(200).json(result.rows);
   } catch (error) {
