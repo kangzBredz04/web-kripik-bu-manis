f34353d6f7f91a4dffdb5078988e8c4f4a552ea2
wahyu <wabredz1234@gmail.com>
2024-07-17 06:28:23 +0700

    update add customer controller

--- server/api/controllers/customer-controller.js
+++ server/api/controllers/customer-controller.js
@@ -2,18 +2,20 @@ import argon2 from "argon2";
 import { pool } from "../config/db.js";
 
 export const addAccountCustomer = async (req, res) => {
-  const { customer_code, username, password, role } = req.body;
+  const { customer_code, name, password, role } = req.body;
   try {
     const hashPassword = await argon2.hash(password);
     const result = await pool.query(
-      "INSERT INTO customers (customer_code, username, password) VALUES ($1, $2, $3) RETURNING *",
-      [customer_code, username, hashPassword]
+      "INSERT INTO customers (customer_code, name, password) VALUES ($1, $2, $3) RETURNING *",
+      [customer_code, name, hashPassword]
     );
+    console.log("masuk");
     res.status(201).json({
       msg: "Pendaftaran akun telah berhasil",
       data: result.rows[0],
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error });
   }
 };
